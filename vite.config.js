d5e730a7fd933eea72e1b3693c55af48a75d6c82
luna-miguel <miguel.luna1024@gmail.com>
2025-05-30 18:06:50 -0400

    Base website

--- vite.config.js
+++ vite.config.js
@@ -5,29 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/\.(png|jpe?g|gif|svg|webp|ico)$/.test(assetInfo.name)) {
-            return `assets/[name][extname]`
-          }
-          if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
-            return `assets/[name][extname]`
-          }
-          if (ext === 'css') {
-            return `assets/[name][extname]`
-          }
-          return `assets/[name][extname]`
-        },
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js',
-      }
-    }
-  }
 })
