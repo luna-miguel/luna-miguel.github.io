578a6afd5ebc206a534f2d67867064db72afb6de
luna-miguel <miguel.luna1024@gmail.com>
2025-05-30 18:45:50 -0400

    Fix asset handling in build process

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 })
